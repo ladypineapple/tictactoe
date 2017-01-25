c7c0b19fca43690cb6461fa01307358e68792af0
ladypineapple <maureenpine@gmail.com>
2017-01-25 11:45:07 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 'use strict';
 
-// user require with a reference to bundle the file and use it in this file
-// var example = require('./example');
+const setAPIOrigin = require('../../lib/set-api-origin');
+const config = require('./config');
 
-// load manifests
-// scripts
-require('./assets/scripts/index.js');
+$(() => {
+  setAPIOrigin(location, config);
+});
 
-// styles
-require('./assets/styles/index.scss');
+// use require with a reference to bundle the file and use it in this file
+// const example = require('./example');
+
+// use require without a reference to ensure a file is bundled
+require('./example');
