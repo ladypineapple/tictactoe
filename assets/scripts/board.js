333300d158799ddc9c2e0d486f95fc710056d99f
ladypinetree <maureenpine@gmail.com>
2017-01-25 16:39:16 -0500

    Corrects locations of boards.js Proper identifier for player markers

--- assets/scripts/board.js
+++ assets/scripts/board.js
@@ -4,8 +4,8 @@ const player = function (name, index) {
   this.name = name;
   this.playerNumber = index;
   if (index === 1) {
-    this.piece = 'X';
+    this.marker= 'X';
   } else {
-    this.piece = 'O';
+    this.marker = 'O';
   }
 };
