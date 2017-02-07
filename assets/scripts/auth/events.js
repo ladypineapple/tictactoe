8b563e05460ba9182cf2e7dced6f35846045864a
ladypinetree <maureenpine@gmail.com>
2017-02-07 18:54:35 -0500

    adds link to config.js comments out catch ui failure in events.js on sign in and sign out

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -8,27 +8,28 @@ const store = require('./store');
 
 const onSignUp = function (event) {
   event.preventDefault();
+
   let data = getFormFields(event.target);
+
   api.signUp(data)
     .then(ui.success)
-    .catch(ui.failure)
+    // .catch(ui.failure)
     ;
 };
 
 const onSignIn = function (event) {
   event.preventDefault();
+
   let data = getFormFields(event.target);
+
   api.signIn(data)
-    .then((response) => {
-      store.user = response.user;
-      return store.user;
-    })
-    .then(ui.success)
-    .then(() => {
-      console.log(store);
-    })
-    .catch(ui.failure)
-    ;
+  .then((response) => {
+    store.user = response.user;
+    return store;
+  })
+  .then(ui.signInSuccess)
+  // .catch(ui.failure)
+  ;
 };
 
 const onChangePassword = function (event) {
